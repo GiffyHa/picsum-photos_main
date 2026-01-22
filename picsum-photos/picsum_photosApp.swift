30baae41cec1a4ff177d662fab0e8d7f8b040df6
Giffy <giffyhering@gmail.com>
2026-01-22 13:15:57 +0700

    Initial Commit

---
+++ picsum-photos/picsum_photosApp.swift
@@ -0,0 +1,17 @@
+//
+//  picsum_photosApp.swift
+//  picsum-photos
+//
+//  Created by giffy on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct picsum_photosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
